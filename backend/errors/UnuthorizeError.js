09c8ff92e3bb9e2cb2be06d44e83158a763c4988
AkiraC0de <ravenakira22@gmail.com>
2026-04-07 21:35:38 +0800

    Refactor: refactored the verifyToken middleware to match with services way of writing and handling errors. Added new UnthorizeError class to better handle the authorization errors.

---
+++ backend/errors/UnuthorizeError.js
@@ -0,0 +1,22 @@
+const GenericError = require('./GenericError');
+const ERROR_CODES = require('../errors/errorCodes');
+
+class UnathorizeError extends GenericError {
+  constructor(message, code = ERROR_CODES.UNUTHORIZE_ERROR) {
+    super(
+      401,
+      message,
+      code,
+    );
+  }
+
+  toJSON() {
+      return {
+          success: false,
+          code: this.code,  
+          message: this.message
+      };
+  }
+}
+
+module.exports = UnathorizeError;
--- backend/middlewares/verifyToken.js
+++ backend/middlewares/verifyToken.js
@@ -1,31 +1,35 @@
 const crypto = require('crypto');
 const Token = require('../models/Token');
+const UnathorizeError = require('../errors/UnuthorizeError');
+const ERROR_CODES = require('../errors/errorCodes');
 
 const verifyToken = async (req, res, next) => {
-  try {
-    const authorization = req.headers.authorization || req.headers.Authorization;
-    if(!authorization) return res.status(403).json({success: false, message: 'You are not authorized to access this!'});
-    
-    if(!authorization.startsWith('Bearer ')) return res.status(403).json({success: false, message: 'You are not authorized to access this!'});
-
-    // Extract the token from authorization
-    const token = authorization.split(' ')[1];
-
-    const hashedToken = crypto
-                          .createHash('sha256')
-                          .update(token)
-                          .digest('hex');
-
-    const validToken = await Token.findOne({token : hashedToken}).populate('user');
-    if(!validToken) return res.status(403).json({success: false, message: 'Invalid or Expired Token'});
-
-    req.user = validToken.user;
-    req.token = validToken;
-    next();
-  } catch (error) {
-    console.log(error.message);
-    return res.status(500).json({success: false, message: 'Server Error'});
+  const authorization = req.headers.authorization || req.headers.Authorization;
+  if(!authorization) {
+    throw new UnathorizeError('Authorization in request headers is required.');
   }
+  
+  if(!authorization.startsWith('Bearer ')) {
+    throw new UnathorizeError('Invalid Authorization format. Valid : Bearer <token>');
+  }
+
+  // Extract the token from authorization
+  const token = authorization.split(' ')[1];
+
+  const hashedToken = crypto
+                        .createHash('sha256')
+                        .update(token)
+                        .digest('hex');
+
+  const validToken = await Token.findOne({token : hashedToken}).populate('user');
+
+  if(!validToken) {
+    throw new UnathorizeError('Token has Expired or is Invalid.', ERROR_CODES.INVALID_TOKEN);
+  }
+
+  req.user = validToken.user;
+  req.token = validToken;
+  next();
 }
 
 module.exports = verifyToken;
