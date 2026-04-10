0227319dbed6d3f4f2c965427155880f57f36b3b
AkiraC0de <ravenakira22@gmail.com>
2026-04-10 16:57:47 +0800

    centralized the constant to its own directory.

---
+++ backend/constants/cookie.js
@@ -0,0 +1,3 @@
+const COOKIE_MAX_AGE = 15 * 24 * 60 * 60 * 1000; // 15 days
+
+module.exports = { COOKIE_MAX_AGE };
---
+++ backend/constants/requiredFields.js
@@ -0,0 +1,31 @@
+const VERIFY_USER_EMAIL_REQUIRED_FIELDS = [ 
+    { field : 'otp', label: 'OTP'}, 
+];
+
+const REGISTER_USER_REQUIRED_FIELDS = [ 
+    { field : 'firstName', label: 'First name'}, 
+    { field : 'lastName', label: 'Last name'}, 
+    { field : 'email', label: 'Email'},
+    { field : 'password', label: 'Password'}
+];
+
+const SIGNIN_REQUIRED_FIELDS = [ 
+    { field : 'email', label: 'Email'}, 
+    { field : 'password', label: 'Password'}, 
+];
+
+const REQUEST_RESET_PASSWORD_REQUIRED_FIELDS = [ 
+    { field : 'email', label: 'Email'}, 
+];
+
+const RESET_PASSWORD_REQUIRED_FIELDS = [ 
+    { field : 'password', label: 'Password'}, 
+];
+
+module.exports = {
+  VERIFY_USER_EMAIL_REQUIRED_FIELDS,
+  SIGNIN_REQUIRED_FIELDS,
+  REGISTER_USER_REQUIRED_FIELDS,
+  REQUEST_RESET_PASSWORD_REQUIRED_FIELDS,
+  RESET_PASSWORD_REQUIRED_FIELDS
+}
